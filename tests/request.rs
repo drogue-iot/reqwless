7dbea704fe9fbdded719d051c3a720ef16724414
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-01-26 09:17:43 +0100

    Fix tests

--- tests/request.rs
+++ tests/request.rs
@@ -1,7 +1,7 @@
 use embedded_io::adapters::FromTokio;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Server};
-use reqwless::request::{ContentType, Request, Response};
+use reqwless::{headers::ContentType, request::Request, response::Response};
 use std::sync::Once;
 use tokio::net::TcpStream;
 use tokio::sync::oneshot;
