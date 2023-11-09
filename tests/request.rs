ce057952b6870762787d8dac8f05a831b4f58acd
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2023-11-09 21:52:04 +0100

    unit test reproducing google panic

--- tests/request.rs
+++ tests/request.rs
@@ -69,3 +69,24 @@ async fn write_without_base_path() {
 
     assert!(from_utf8(&buf).unwrap().starts_with("GET /hello HTTP/1.1"));
 }
+
+#[tokio::test]
+async fn google_panic() {
+    use std::net::SocketAddr;
+    let google_ip = [142, 250, 74, 110];
+    let addr = SocketAddr::from((google_ip, 80));
+
+    let conn = tokio::net::TcpStream::connect(addr).await.unwrap();
+    let mut conn = embedded_io_adapters::tokio_1::FromTokio::new(conn);
+
+    let request = Request::get("/")
+        .host("www.google.com")
+        .content_type(ContentType::TextPlain)
+        .build();
+    request.write(&mut conn).await.unwrap();
+
+    let mut rx_buf = [0; 1024];
+    let resp = Response::read(&mut conn, Method::GET, &mut rx_buf).await.unwrap();
+    let body = resp.body().read_to_end().await.unwrap();
+    println!("{} -> {}", body.len(), core::str::from_utf8(&body).unwrap());
+}
